c744f4c6f9b700526a6dc9077c1666fe07ce8a47
Hasith Gamlath <hasithgamlath327@gmail.com>
2025-09-24 16:55:56 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingRoot: "/Users/hasith/Desktop/hello/projects/TutorLink - Smart Student-Tutor Platform/TutorLink - Frontend",
 };
 
 export default nextConfig;
